7449674bf7e8d9658c77c36f4ab933d61b92d459
ArceseN <50177994+ArceseN@users.noreply.github.com>
2019-06-06 13:27:54 -0400

    Add files via upload

---
+++ CustomerInformation.java
@@ -0,0 +1,44 @@
+package customerProfile;
+
+import java.util.*;
+
+public class CustomerInformation {
+	
+	//creating an arraylist to store the customer's personal information
+	
+	 static ArrayList<String> personalInfo = new ArrayList<String>();
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+	 Scanner input = new Scanner(System.in);
+     
+	 System.out.println("Full Name: ");
+     String name = input.nextLine();
+     personalInfo.add(name);
+     
+     System.out.println("Address: ");
+     String address = input.nextLine();
+     personalInfo.add(address);
+     
+     System.out.println("Phone Number (xxx)xxx-xxxx: ");
+     String phone = input.nextLine();
+     personalInfo.add(phone);
+     
+     System.out.println("Email Address: ");
+     String email = input.nextLine();
+     personalInfo.add(email);
+     
+	
+//     while (input.hasNextLine()) {
+//    	 personalInfo.add(input.nextLine());
+//    
+//     }
+ System.out.println(personalInfo);
+ 
+	}
+ }
+
+
+
