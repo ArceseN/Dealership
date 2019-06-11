fc841c080747a7a01c34e7d6abbc87f438cba82f
anthony.bowen <shdwdncr@gmail.com>
2019-06-11 11:21:38 -0400

    Appointment maker

---
+++ ApptDate.java
@@ -0,0 +1,21 @@
+package apptProject;
+
+import java.util.Scanner;
+
+
+public class ApptDate {
+	
+	static Scanner scText = new Scanner(System.in); 
+	   public static void ClientAppt() {
+		   
+		    System.out.println("Please enter date for appointment: "); // Enter a test date
+		    String tDate = scText.nextLine();
+		    System.out.println("date entered was: " + tDate);  
+		    
+		    
+		    
+		    
+	    Scanner in,close;
+ }
+
+}
