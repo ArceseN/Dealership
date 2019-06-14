1951e78011addb98fa1a62df8b1e4b09803625a5
FyrWalkyr60 <poppipelli@gmail.com>
2019-06-14 12:00:57 -0400

    Updated DeptQueue
    Semi-completed code, comments needed.

--- src/dealership/deptQueue.java
+++ src/dealership/deptQueue.java
@@ -6,21 +6,24 @@ import java.util.Queue;
 
 public class deptQueue {
 	Scanner input = new Scanner(System.in);
-	
+	String dqinfo = input.nextLine();
+	public void deptQueueAdd() {
 	Queue<String> sd = new LinkedList<String>();
 	Queue<String> fd = new LinkedList<String>();
 	Queue<String> ld = new LinkedList<String>();
 	
 	System.out.println("Please enter the requested department: Sales, Finance, or Leasing");
 	
-	if (input = "Sales") {
-		sd.add(Client());
-	} else if(input = "Finance") {
-		fd.add(Client());
-	} else if (input = "Leasing") {
-		ld.add(Client());
+	if (dqinfo == "Sales") {
+		sd.addAll(ClientInfo.personalInfo);
+	}
+	 if(dqinfo == "Finance") {		 
+	 	fd.addAll(ClientInfo.personalInfo);
+	 }
+	 if (dqinfo == "Leasing") {
+		ld.addAll(ClientInfo.personalInfo);
 	}
 	
-	}
+  }
 
 }
